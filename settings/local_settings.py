0b63b8279694a6f8b6d7944b50db32b56d6b905d
o5b <o5b.developer@gmail.com>
2024-01-23 21:12:06 +0200

    Added local_settings.py for example (should be removed in development)

---
+++ settings/local_settings.py
@@ -0,0 +1,21 @@
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+SECRET_KEY = 'django-insecure-pkp&df0v!a&4+@gb*&%9bj5bej4(o&nea*llhj)s03cw8x!ow^'
+
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
+}
+
+
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'tmp/app-messages')
